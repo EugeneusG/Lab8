04be375e9491cd34339fe19b61963d17cc4a4b99
EugeneusG <zhenya9911@mail.ru>
2020-05-25 17:24:22 +0300

    Добавьте файлы проекта.

---
+++ зад4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace зад4
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = "Разбиение строки на несколько частей";
+            string[] b = a.Split();
+            Array.Sort(b);
+            a = String.Join(' ', b);
+            Console.WriteLine(a);
+        }
+    }
+}
